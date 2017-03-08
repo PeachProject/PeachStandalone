224677c82b6fcf9498c57651766fa3b8e43b1e66
Henry Müssemann <h.muessemann@gmx.de>
2017-03-08 12:14:38 +0100

    Curling node setup works now. Ref T26

--- setup/pre_venv/install_npm.py
+++ setup/pre_venv/install_npm.py
@@ -4,6 +4,6 @@ def do():
     import subprocess
     subprocess.call(["sudo", "apt-get", "update"])
     setup_utilities.apt_install("curl")
-    subprocess.call(["curl", "-sL", "https://deb.nodesource.com/setup_6.x", "|", "sudo", "-E", "bash", "-"])
+    subprocess.call("curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -", shell=True)
     subprocess.call(["sudo", "apt-get", "install", "-y", "nodejs"])
     setup_utilities.log("Successfully installed npm!")
